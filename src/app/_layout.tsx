d1417d185cc7d5c6a7260eb55646ab08d579cd23
Yushan Fernando <yushanwebdev@gmail.com>
2024-03-01 13:12:09 +0530

    feat: Nativewind Setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,6 +10,7 @@ import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
 import { useColorScheme } from "@/components/useColorScheme";
+import "./global.css";
 
 export {
   // Catch any errors thrown by the Layout component.
